35b1ed35cab2f6e7fbce6c7ced0a4e153bbda33e
SPStudioWebsites <simon.poeske@gmail.com>
2026-05-07 11:53:37 +0200

    neuer Name für Blog

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       { source: "/agb", destination: "/impressum", permanent: true },
+      {
+        source: "/blog/warum-dein-lokal-bei-google-unsichtbar-ist",
+        destination: "/blog/website-fuer-restaurants-und-cafes-in-hassfurt",
+        permanent: true,
+      },
     ];
   },
   experimental: {
